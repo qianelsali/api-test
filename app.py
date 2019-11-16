825eda684b8bd75fd94140612e0010d5c7135aca
qianelsali <onehorselee@gmail.com>
2019-11-15 22:57:33 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from flask import Flask, request, Response, render_template, jsonify
 app = Flask(__name__)
 
 @app.route("/homepage")
-def hello_world():
-	return "Welcome to my Service!"
+def home_page():
+	return "Welcome to wokedudodo's home page!"
 
+@app.route("/")
+def hello_world():
+	return "Welcome to wokedudodo Service!"
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
