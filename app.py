4f2adb68966633f3071f392af213a830f0db3bf9
qianelsali <onehorselee@gmail.com>
2019-11-16 09:10:22 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/homepage")
 def home_page():
-	return "Welcome to wokedudodo's home page!"
+	return "Welcome to chuckedudodu's home page!"
 
 @app.route("/")
 def hello_world():
-	return "Welcome to wokedudodo Service!"
+	return "Welcome to chuckedudodu Service!"
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=80, debug=True)
