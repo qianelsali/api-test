28ae39bcceb71511524dcc509192539cef271a9a
qianelsali <onehorselee@gmail.com>
2019-11-16 10:08:35 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+'''
 from flask import Flask
 
 app = Flask(__name__)
@@ -12,3 +13,14 @@ def hello_world():
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=80, debug=True)
+	
+'''	
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Welcome to chuckedudodu Service!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
